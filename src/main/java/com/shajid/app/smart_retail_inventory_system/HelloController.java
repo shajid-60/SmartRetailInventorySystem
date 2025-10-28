98f9e7d2777e9adce7ba6037917bf726dc57d784
MAJHARUL SHAJID <shajid2207060@stud.kuet.ac.bd>
2025-10-28 20:59:21 +0600

    Initializing Commit

---
+++ src/main/java/com/shajid/app/smart_retail_inventory_system/HelloController.java
@@ -0,0 +1,14 @@
+package com.shajid.app.smart_retail_inventory_system;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ src/main/java/com/shajid/app/smart_retail_inventory_system/Launcher.java
@@ -0,0 +1,9 @@
+package com.shajid.app.smart_retail_inventory_system;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(HelloApplication.class, args);
+    }
+}
